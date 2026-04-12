bed2fba87f95c6a668471cbaf22a25f8c51edbe9
Gangadhar Kuratti <gangadharkuratti549@gmail.com>
2026-04-12 23:38:22 +0530

    Add files via upload

---
+++ practice_problems_10.py
@@ -0,0 +1,16 @@
+# program to print the armstrong number
+
+num = int(input("Enter a number: "))
+temp = num
+digits = len(str(num))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** digits
+    temp =temp // 10
+
+if sum == num:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
